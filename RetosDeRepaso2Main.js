f2503051893827b00b38aa7bf857a4f8c2fd80ff
JMarSar <marsarjorge@gmail.com>
2021-10-17 20:44:25 +0200

    subida de archivos

---
+++ RetosDeRepaso2Main.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var RetosDeRepaso2_1 = require("./RetosDeRepaso2");
+var Nokia3210 = new RetosDeRepaso2_1.Mobile("Nokia 3210", "3210", "Nokia", 5, "Blue", false, 1, 30);
+var Iphone3G = new RetosDeRepaso2_1.Mobile("Iphone 3G", "3G", "Apple", 60, "Grey", false, 1, 50);
+var SamsunGalaxy10 = new RetosDeRepaso2_1.Mobile("Samsung Galaxy 10", "Galaxy 10", "Samsung", 125, "Black", true, 2, 300);
+console.log(Nokia3210);
+console.log(Iphone3G);
+console.log(SamsunGalaxy10);
+Nokia3210.is5G = true;
+Nokia3210.cameraNumber = 4;
+console.log(Nokia3210);
+console.log(Iphone3G);
+console.log(SamsunGalaxy10);
---
+++ RetosDeRepaso2Main.ts
@@ -0,0 +1,16 @@
+import { Mobile } from './RetosDeRepaso2';
+
+let Nokia3210 = new Mobile("Nokia 3210", "3210", "Nokia", 5, "Blue", false, 1, 30)
+let Iphone3G = new Mobile("Iphone 3G", "3G", "Apple", 60, "Grey", false, 1, 50)
+let SamsunGalaxy10 = new Mobile("Samsung Galaxy 10", "Galaxy 10", "Samsung", 125, "Black", true, 2, 300)
+
+console.log(Nokia3210)
+console.log(Iphone3G)
+console.log(SamsunGalaxy10)
+
+Nokia3210.is5G = true
+Nokia3210.cameraNumber = 4
+
+console.log(Nokia3210)
+console.log(Iphone3G)
+console.log(SamsunGalaxy10)
