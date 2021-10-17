30867bad1b631b4c2b64d881a0b12b3a20ba7d9b
JMarSar <marsarjorge@gmail.com>
2021-10-17 21:15:27 +0200

    0

--- RetosDeRepaso2Main.ts
+++ RetosDeRepaso2Main.ts
@@ -8,8 +8,8 @@ console.log(Nokia3210)
 console.log(Iphone3G)
 console.log(SamsunGalaxy10)
 
-Nokia3210.is5G = true
-Nokia3210.cameraNumber = 4
+Nokia3210.setIs5G(true)
+Nokia3210.setCameraNumber(4)
 
 console.log(Nokia3210)
 console.log(Iphone3G)
